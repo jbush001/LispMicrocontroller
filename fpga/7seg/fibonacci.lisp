3ac79e4eafe70f2bde9adcc52e2158157e188c0b
Jeff Bush <jeffbush001@gmail.com>
2012-07-22 21:14:58 -0700

    Add license header

--- fpga/7seg/fibonacci.lisp
+++ fpga/7seg/fibonacci.lisp
@@ -1,3 +1,19 @@
+; 
+; Copyright 2011-2012 Jeff Bush
+; 
+; Licensed under the Apache License, Version 2.0 (the "License");
+; you may not use this file except in compliance with the License.
+; You may obtain a copy of the License at
+; 
+;     http://www.apache.org/licenses/LICENSE-2.0
+; 
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS,
+; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+; See the License for the specific language governing permissions and
+; limitations under the License.
+; 
+
 ;
 ; On the cyclone II dev board, compute nth iteration of a fibonacci sequence and display
 ; on 7 segment display
