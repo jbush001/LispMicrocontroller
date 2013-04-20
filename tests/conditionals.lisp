12b9466ed9dbc7156216d92b1707010072a3a1ce
Jeff Bush <jeffbush001@gmail.com>
2013-04-20 11:08:35 -0700

    Add test for conditionals

---
+++ tests/conditionals.lisp
@@ -0,0 +1,25 @@
+(assign a 1)
+(assign b 0)
+
+(if (and a a a a a)
+	(printchar 65)
+)
+
+(if (and a a b a a)
+	(printchar 66)
+)
+
+(if (or b b b b a)
+	(printchar 67)
+)
+
+(if (or a a)
+	(printchar 68)
+)
+
+(if (and b b)
+	(printchar 69)
+)
+
+
+	
