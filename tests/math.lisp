152ece44c4d3ac729feb3691b068c988817542a3
Jeff Bush <jeffbush001@gmail.com>
2016-12-27 21:10:03 -0800

    Add more math tests

--- tests/math.lisp
+++ tests/math.lisp
@@ -18,26 +18,51 @@
 (assign NEG -7)
 (assign POS 23)
 
-(print (* NEG NEG))     ; CHECK: 49
-(print (* NEG POS))     ; CHECK: -161
-(print (* POS POS))     ; CHECK: 529
-(print (* POS NEG))     ; CHECK: -161
-(print (* 0 POS))       ; CHECK: 0
-(print (* POS 0))       ; CHECK: 0
+; Builtin operators
+(print (+ POS NEG))             ; CHECK: 16
+(print (- POS NEG))             ; CHECK: 30
+(print (> 23 POS))              ; CHECK: 0
+(print (> 24 POS))              ; CHECK: 1
+(print (< 23 POS))              ; CHECK: 0
+(print (< 22 POS))              ; CHECK: 1
+(print (>= 22 POS))             ; CHECK: 0
+(print (>= 23 POS))             ; CHECK: 1
+(print (>= 24 POS))             ; CHECK: 1
+(print (<= 24 POS))             ; CHECK: 0
+(print (<= 23 POS))             ; CHECK: 1
+(print (<= 22 POS))             ; CHECK: 1
+(print (= 22 POS))              ; CHECK: 0
+(print (= 23 POS))              ; CHECK: 1
+(print (<> 22 POS))             ; CHECK: 1
+(print (<> 23 POS))             ; CHECK: 0
+(print (bitwise-and NEG POS))   ; CHECK: 17
+(print (bitwise-or 64 POS))     ; CHECK: 87
+(print (bitwise-xor POS 15))    ; CHECK: 24
+(print (rshift POS 1))          ; CHECK: 11
+(print (rshift POS 2))          ; CHECK: 5
+(print (lshift POS 1))          ; CHECK: 46
+(print (lshift POS 2))          ; CHECK: 92
 
-(print (/ -2317 POS))   ; CHECK: -100
-(print (/ -2317 NEG))   ; CHECK: 331
-(print (/ 2317 POS))    ; CHECK: 100
-(print (/ 2317 NEG))    ; CHECK: -331
-(print (/ POS 2317))    ; CHECK: 0
-(print (/ 0 POS))       ; CHECK: 0
+; Runtime library functions
+(print (* NEG NEG))             ; CHECK: 49
+(print (* NEG POS))             ; CHECK: -161
+(print (* POS POS))             ; CHECK: 529
+(print (* POS NEG))             ; CHECK: -161
+(print (* 0 POS))               ; CHECK: 0
+(print (* POS 0))               ; CHECK: 0
 
-(print (mod -2319 POS)) ; CHECK: 19
-(print (mod -2319 NEG)) ; CHECK: -2
-(print (mod 2319 POS))  ; CHECK: 19
-(print (mod 2319 NEG))  ; CHECK: -2
-(print (sqrt 1902))     ; CHECK: 43
+(print (/ -2317 POS))           ; CHECK: -100
+(print (/ -2317 NEG))           ; CHECK: 331
+(print (/ 2317 POS))            ; CHECK: 100
+(print (/ 2317 NEG))            ; CHECK: -331
+(print (/ POS 2317))            ; CHECK: 0
+(print (/ 0 POS))               ; CHECK: 0
 
-(print (abs NEG))       ; CHECK: 7
-(print (abs POS))       ; CHECK: 23
+(print (mod -2319 POS))         ; CHECK: 19
+(print (mod -2319 NEG))         ; CHECK: -2
+(print (mod 2319 POS))          ; CHECK: 19
+(print (mod 2319 NEG))          ; CHECK: -2
+(print (sqrt 1902))             ; CHECK: 43
 
+(print (abs NEG))               ; CHECK: 7
+(print (abs POS))               ; CHECK: 23
